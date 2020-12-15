dc6c60a134d6acda6b4d7fe0ef9046ed1ebb862d
rdroe <d@vidroe.com>
2020-12-15 13:01:52 -0600

    image example; stub for horizontal function; paralell scrolling

--- js/vrugan.js
+++ js/vrugan.js
@@ -1,33 +1,80 @@
 import { invariant, qs, tr } from '/js/util/util.js'
+const PERP = 1
+const PARA = 0
 export const vrugan = (f) => {
   const lib = {
-    updateChild(par, chVrug) {
+    verticalMove(par, chVrug) {
 
       const chParams = par.getChild(chVrug)
       const ch = chVrug.elem
-
       return (ev) => {
-
+          console.log('ev',ev)	    
+	// for logging, assign new scroll position to data element
         par.elem.dataset.scrollTop = par.scrollTop
+
         invariant( () => ev.type === 'scroll' )
         const { range, factor, direction } = chParams
+
+	  if (direction === PERP) {
         if (par.scrollTop > range[0] && par.scrollTop < range[1]) {
           const diff = par.scrollTop - range[0]
-          if (direction === 1) {
-
-            ch.dataset.scrollTop = tr(diff * factor)
-            ch.scrollLeft = tr(diff * factor)
-          }
-        } else if (par.scrollTop <= range[0]) {
+            ch.dataset.scrollLeft = tr(diff * factor)
+            ch.scrollLeft = ch.dataset.scrollLeft
+         } else if (par.scrollTop <= range[0]) {
             ch.dataset.scrollTop = 0
             ch.scrollLeft = 0
         } else if (par.scrollTop >= range[1]) {
+            ch.dataset.scrollLeft = tr((range[1] - range[0]) * factor)
+            ch.scrollLeft =  ch.dataset.scrollLeft
+        }
+	  } else if (direction === PARA) {
+          const diff = par.scrollTop - range[0]
+            ch.dataset.scrollTop = tr(diff * factor)
+            ch.scrollTop = ch.dataset.scrollTop
+         } else if (par.scrollTop <= range[0]) {
+            ch.dataset.scrollTop = 0
+            ch.scrollTop = 0
+        } else if (par.scrollTop >= range[1]) {
             ch.dataset.scrollTop = tr((range[1] - range[0]) * factor)
-            ch.scrollLeft = tr((range[1] - range[0])  * factor)
+            ch.scrollTop = ch.dataset.scrollTop 
+        }
+
+
+	  }
+    },
+  
+    horizontalMove(par, chVrug){
+
+      const chParams = par.getChild(chVrug)
+      const ch = chVrug.elem
+      return (ev) => {
+          console.log('ev',ev)	    
+	// for logging, assign new scroll position to data element
+        par.elem.dataset.scrollLeft = par.scrollLeft
+
+        invariant( () => ev.type === 'scroll' )
+        const { range, factor, direction } = chParams
+        if (par.scrollLeft > range[0] && par.scrollLeft < range[1]) {
+          const diff = par.scrollLeft - range[0]
+          if (direction === PERP) {
+
+            ch.dataset.scrollLeft = tr(diff * factor)
+            ch.scrollTop = tr(diff * factor)
+          } else {
+            
+	  }
+        } else if (par.scrollLeft <= range[0]) {
+            ch.dataset.scrollLeft = 0
+            ch.scrollTop = 0
+        } else if (par.scrollLeft >= range[1]) {
+            ch.dataset.scrollLeft = tr((range[1] - range[0]) * factor)
+            ch.scrollTop = tr((range[1] - range[0])  * factor)
         }
       }
-    },
-  }
+    
+    }
+  
+}
   const elem = qs(f)
   const scroll = Object.create({
     getChild(chElem) {
@@ -38,11 +85,17 @@ export const vrugan = (f) => {
     get scrollTop () {
       return elem.scrollTop
     },
-    add(chSel, {range, factor }) {
-      const ch = vrugan(chSel)
-      if (this.children.has(ch)) return
-      this.children.set(ch, { range, factor, direction: 1 })
-      this.elem.addEventListener('scroll', lib.updateChild(this, ch))
+      add(chSel, { range, factor, direction = PERP, sensitiveTo = 'v' }) {
+          const ch = vrugan(chSel)
+	  if (this.children.has(ch)) return
+	  // track this child by placing it in parent's set.  
+          this.children.set(ch, { range, factor, direction })
+	  // add the listener	  
+          if (sensitiveTo === 'v') {
+	    this.elem.addEventListener('scroll', lib.verticalMove(this, ch))
+	  } else if (sensitiveTo === 'h') {
+ 	    this.elem.addEventListener('scroll', lib.horizontalMove(this, ch))           
+	  } else throw new TypeError('The argument must be "h" or "v".')
 
     },
     updateToParent(parentEvent) {
