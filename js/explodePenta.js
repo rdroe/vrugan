6db087d7bcb4769fc2af5ade1a69f98ecae7375c
rdroe <d@vidroe.com>
2020-12-29 22:25:09 -0600

    export more penta code

--- js/explodePenta.js
+++ js/explodePenta.js
@@ -31,3 +31,33 @@ export default (pentas, pos, obj, start, max = 100000) => {
     }
     pentaHelper(pentas, translate, start, max)
 }
+
+const updatePentaDom = (el, deg, colorOpac) => {
+    el.style.transform = `rotateZ(${deg}deg)`
+}
+
+const spinHelper = (pos, ps, pe) => {
+    const modPos1 = pos % (360 * 150)
+    const multiplier = tr(modPos1 / (360 * 10) * 360 * 100) / 100
+    return multiplier
+}
+
+export const rotatePenta = (pos, obj) => {
+
+    const el = obj.el
+
+    const ps = obj.get('pixels', 'ps')
+    const pe = obj.get('pixels', 'pe')
+    let spinParams
+
+    if (pos < ps) {
+        spinParams = spinHelper(ps, ps, pe)
+    } else if (pos > pe) {
+        spinParams = spinHelper(pe, ps, pe)
+    } else {
+        spinParams = spinHelper(pos, ps, pe)
+    }
+
+    // rotate only
+    updatePentaDom(el, spinParams)
+}
