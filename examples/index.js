b143b9bad387f9b9870cd49343d0573b600877f3
rdroe <d@vidroe.com>
2021-04-03 20:24:25 -0500

    runnable examples cleanup etc

--- examples/index.js
+++ examples/index.js
@@ -2,9 +2,13 @@ const finalhandler = require('finalhandler')
 const http = require('http')
 const serveStatic = require('serve-static')
 const path = require('path')
+const port = 8080
 
-const assets = serveStatic(path.join(__dirname, '..'))
+const assets = serveStatic(path.join(__dirname, '.'))
 const server = http.createServer(function onRequest(req, res, next) {
     assets(req, res, finalhandler(req, res))
 })
-server.listen(8080)
+
+server.listen(port)
+server.on('listening', () => console.log(`Serving on ${port}`))
+
